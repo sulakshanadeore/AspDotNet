f66e5e500feb472f11fc59450bb0d7c20b5fef81
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-05-24 15:05:16 +0530

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace WebApplication3
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Application["hits"] = 0;
+        }
+
+
+        void Session_Start(object sender, EventArgs e)
+        {
+            int i=Convert.ToInt32(Application["hits"]);
+            i = i + 1;
+            Application["hits"] = i;
+
+        }
+        void Session_End(object sender, EventArgs e)
+        {
+            
+          //  Session.Contents.Clear();
+            //Session.Remove();--- removing the key, so value will be deleted
+           // Session.RemoveAll();
+            Session.Clear();//Removes key-value pairs from the session collection
+            Session.Abandon();//Cancels the session
+        }
+
+        void Application_End(object sender, EventArgs e)
+        {
+            Application["hits"] = 0;
+        }
+    }
+}
---
+++ WebForm3.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("Request URL= " + HttpContext.Current.Request.Url + "<br/>");
+            Response.Write("Session Count= " + HttpContext.Current.Session.Count+ "<br/>");
+            Response.Write("Current Time stamp= " + HttpContext.Current.Timestamp + "<br/>");
+            Response.Write("Is debug enabled = " + HttpContext.Current.IsDebuggingEnabled + "<br/>");
+            Response.Write("Username  = " + HttpContext.Current.User.Identity.Name + "<br/>");
+            Response.Write("Mode  = " + HttpContext.Current.User.Identity.AuthenticationType + "<br/>");
+
+        }
+    }
+}
