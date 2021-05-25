d5b13bd33b3dd423a1eace02e6b82e2e85ac258a
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-05-25 17:43:54 +0530

    Add files via upload

--- MainPage.Master.designer.cs
+++ MainPage.Master.designer.cs
@@ -112,5 +112,23 @@ namespace MasterPagesDemo
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+
+        /// <summary>
+        /// ContentPlaceHolder2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder2;
+
+        /// <summary>
+        /// AdRotator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.AdRotator AdRotator1;
     }
 }
