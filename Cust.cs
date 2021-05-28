dca42515e24405d7a8768d1fc1ff013466f6e2e8
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-05-28 12:42:49 +0530

    Add files via upload

---
+++ Cust.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace StateMgtMVC.Models
+{
+    public class Cust
+    {
+        [Required(ErrorMessage ="Custid required"]
+        public int Custid { get; set; }
+        [MaxLength(20,ErrorMessage ="Name cannot be greater than 20 chars")]
+        public string Custname { get; set; }
+        
+        public string City { get; set; }
+        [EmailAddress(ErrorMessage ="Enter valid emaild id ")]
+        public string CustEmail { get; set; }
+    }
+}
