b36e6a1caa2d5d3ad6dfdb57a4b8f6148679d717
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-05-31 13:20:38 +0530

    Add files via upload

---
+++ IService1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        int AddNos(int no1, int no2);
+
+        [OperationContract]
+        bool PayService(int amt);
+
+        [OperationContract]
+        bool InsertProduct(Product p);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class Product
+    {
+        private int ProdID;
+
+        [DataMember]
+        public int ProductID
+        {
+            get { return ProdID; }
+            set { ProdID = value; }
+        }
+
+
+        private string ProdName;
+        [DataMember]
+        public string ProductName
+        {
+            get { return ProdName; }
+            set { ProdName = value; }
+        }
+
+        private int Price;
+        [DataMember]
+        public int ProductPrice
+        {
+            get { return Price; }
+            set { Price = value; }
+        }
+
+        private string Qty;
+        [DataMember]
+        public string ProductQty
+        {
+            get { return Qty; }
+            set { Qty = value; }
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using ConnectedBAL;
+using ConnectedDAL;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public int AddNos(int no1, int no2)
+        {
+            return no1 + no2;
+        }
+
+        public bool InsertProduct(Product p)
+        {
+            bool status=false;
+            ConnectedDAL.ProductsDAL dal = new ProductsDAL();
+            ConnectedBAL.Products p1 = new Products();
+           // p1.ProductID = p.ProductID;
+            p1.ProductName = p.ProductName;
+            p1.UnitPrice = p.ProductPrice;
+            p1.QtyPerUnit = p.ProductQty;
+            p1.CategoryID = 1;
+            p1.SupplierID = 1;
+            status=dal.InsertProduct(p1);
+            //Dal Code
+            return status;
+        }
+
+        public bool PayService(int amt)
+        {
+            bool status = true;
+            //Dal Code
+            return status;
+            //throw new NotImplementedException();
+        }
+    }
+}
