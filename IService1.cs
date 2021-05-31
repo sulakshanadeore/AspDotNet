9f0248f1877dad8a3e20f0b9342c2ed1dae1dac2
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-05-31 17:03:46 +0530

    Add files via upload

---
+++ IService1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCF_Demo
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        //[OperationContract]
+        //string GetData(int value);
+        //[OperationContract]
+        //int AddNos(int i, int j);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "CustomersJson", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        Customer GetDataUsingDataContract();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "CustomersXml", ResponseFormat = WebMessageFormat.Xml, RequestFormat = WebMessageFormat.Xml)]
+        Customer GetDataUsingDataContractXML();
+
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class Customer
+    {
+        
+        string custName = "Hello ";
+        [DataMember]
+        public string CustName { get; set; }
+    }
+}
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WCFClientDemo.ServiceReference1;
+namespace WCFClientDemo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //ServiceReference1.Service1Client proxy = new Service1Client();
+            //int ans=proxy.AddNos(10, 20);
+            //TextBox1.Text = ans.ToString();
+            ////string a=proxy.GetData(10);
+            
+            
+            WebClient client = new WebClient();
+
+            client.BaseAddress = "http://localhost:63002/Service1.svc/";
+           
+
+            Customer c2 = new Customer();
+            c2.CustName = "Prithvi";
+            string jsondata = JsonConvert.SerializeObject(c2);
+
+            string data = client.DownloadString("http://localhost:63002/Service1.svc/CustomersJson");
+            
+            Customer d =JsonConvert.DeserializeObject<Customer>(data);
+            TextBox2.Text = d.CustName;
+            //Customer obj=proxy.GetDataUsingDataContract(c1);
+            //TextBox2.Text = obj.CustName;
+        }
+    }
+}
